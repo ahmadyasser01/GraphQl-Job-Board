609c63ce30f25da0022c14159786e1c85fbc5e66
ahmadyasser01 <ahmadyasser2814@gmail.com>
2023-03-01 22:14:22 +0200

    job-company association

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,6 +1,10 @@
-import { Job } from "./db.js";
+import { Company, Job } from "./db.js";
 export const resolvers = {
   Query: {
     jobs: () => Job.findAll(),
+  },
+  Job: {
+    // job here is the parent
+    company: (job) => Company.findById(job.companyId),
   },
 };
