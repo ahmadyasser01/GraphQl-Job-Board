87ac20c8d22d78a970a9e24c46474b50917d0164
ahmadyasser01 <ahmadyasser2814@gmail.com>
2023-03-02 20:37:57 +0200

    fetch jobs in jobBoard

--- client/src/components/JobBoard.js
+++ client/src/components/JobBoard.js
@@ -1,12 +1,17 @@
-import JobList from './JobList';
-import { jobs } from '../fake-data';
+import JobList from "./JobList";
+import { jobs } from "../fake-data";
+import { useEffect, useState } from "react";
+import { getJobs } from "../graphql/queries";
 
 function JobBoard() {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    getJobs().then(setJobs);
+  }, []);
   return (
     <div>
-      <h1 className="title">
-        Job Board
-      </h1>
+      <h1 className='title'>Job Board</h1>
       <JobList jobs={jobs} />
     </div>
   );
