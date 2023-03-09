f523f535dbb2e89db4c316cf3a8748f73b9c0282
ahmadyasser01 <ahmadyasser2814@gmail.com>
2023-03-09 17:27:26 +0200

    add fragments

--- client/src/graphql/queries-string.js
+++ client/src/graphql/queries-string.js
@@ -1,15 +1,20 @@
 import { gql } from "@apollo/client";
-
+export const JOB_DETAIL_FRAGMENT = gql`
+  fragment JobDetail on Job {
+    id
+    title
+    company {
+      id
+      name
+    }
+    description
+  }
+`;
 export const JOB_QUERY = gql`
   query JobQuery($id: ID!) {
     job(id: $id) {
-      id
-      title
-      company {
-        id
-        name
-      }
-      description
+      ...JobDetail
     }
   }
+  ${JOB_DETAIL_FRAGMENT}
 `;
