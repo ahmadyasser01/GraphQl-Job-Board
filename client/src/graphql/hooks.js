62632ed8925bb3cc6d6ffeed988439c388810060
ahmadyasser01 <ahmadyasser2814@gmail.com>
2023-03-09 17:51:42 +0200

    add useJob query

--- client/src/graphql/hooks.js
+++ client/src/graphql/hooks.js
@@ -1,5 +1,5 @@
 import { useQuery } from "@apollo/client";
-import { JOBS_QUERY } from "./queries-string";
+import { JOBS_QUERY, JOB_QUERY } from "./queries-string";
 
 export function useJobs() {
   const { data, loading, error } = useQuery(JOBS_QUERY, {
@@ -8,6 +8,15 @@ export function useJobs() {
 
   return {
     jobs: data?.jobs,
+    loading,
+    error: Boolean(error),
+  };
+}
+
+export function useJob(id) {
+  const { data, loading, error } = useQuery(JOB_QUERY, { variables: { id } });
+  return {
+    job: data?.job,
     loading,
     error: Boolean(error),
   };
